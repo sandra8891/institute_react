099fe69e4a1dd91fd1ecd82ecc7a5dfa277b0ea8
sandra8891 <sandraks8891@gmail.com>
2025-04-25 17:38:10 +0530

    media

--- mypro/mypro/urls.py
+++ mypro/mypro/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from myapp.views import CourseViewSet
+
 
 router=DefaultRouter()
 router.register(r'courses',CourseViewSet,basename='course')
@@ -26,3 +29,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include(router.urls))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
